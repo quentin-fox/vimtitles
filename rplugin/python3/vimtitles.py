d212b3236c991d4e1e190b79fa2dfdfe72bca65f
Quentin Fox <quentinbennettfox@gmail.com>
2019-09-14 00:59:21 -0400

    smush all py into one file

--- rplugin/python3/vimtitles.py
+++ rplugin/python3/vimtitles.py
@@ -1,7 +1,9 @@
 import pynvim
 import pathlib
 import datetime
-from player import Player
+import subprocess
+import json
+
 
 @pynvim.plugin
 class VimtitlesPlugin(object):
@@ -16,6 +18,60 @@ class VimtitlesPlugin(object):
         ln = ts[0] - 1
         self.nvim.funcs.echo(buffer[ln])
 
+
+class Player:
+    """class for the mpv player, has controls and can get info about the player"""
+
+    def __init__(self, file):
+        """requires an absolute path to the file?"""
+        self.file = file
+        self.pause = True
+
+    def send_command(self, command):
+        """generic method for sending a command to the player"""
+        if isinstance(command, dict):
+            inputstr = json.dumps(command)
+        elif isinstance(command, str):
+            inputstr = command
+        ps = subprocess.Popen(('echo', inputstr), stdout=subprocess.PIPE)
+        output = subprocess.check_output(('socat', '-', '/tmp/mpvsocket'), stdin=ps.stdout)
+        ps.wait()
+        return(output)
+
+    def play(self, geometry):
+        """initiates the player the file, depending on the filetype"""
+        mpvargs = ('mpv',
+                   self.file,
+                   '--input-ipc-server=/tmp/mpvsocket',
+                   '--really-quiet',  # prevents text being sent via stdout
+                   '--geometry=' + geometry)  # geometry can be 50%x50%, for example
+        subprocess.Popen(mpvargs, close_fds=True, shell=False, stdout=open('stdout.txt', 'w'))
+
+    def cycle_pause(self):
+        """cycles between play and pause"""
+        self.send_command('cycle pause')
+        self.pause = not self.pause  # used to toggle the pause state
+
+    def seek(self, seconds):
+        """scans the player by a number of seconds"""
+        sec_str = str(seconds)
+        seek_dict = {"command": ["seek", sec_str, "relative"]}
+        self.send_command(seek_dict)
+
+    def get_time(self):
+        """returns the current timestamp of the player in decimal seconds"""
+        get_time = {"command": ["get_property", "playback-time"]}
+        time_dict = json.loads(self.send_command(get_time))
+        return(time_dict['data'])
+
+    def seek_abs(self, seconds):
+        """goes to a specific timepoint of the player, in decimal seconds"""
+        sec_str = str(seconds)
+        seek_dict_abs = {"command": ["seek", sec_str, "absolute"]}
+        self.send_command(seek_dict_abs)
+
+    def quit(self):
+        self.send_command('quit')
 
 
 def convert_time(input):
