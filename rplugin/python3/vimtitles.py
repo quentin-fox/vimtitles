56c992c6fd25e3073fae1d021eae3766d3d996d8
Quentin Fox <quentinbennettfox@gmail.com>
2019-09-14 00:54:14 -0400

    fixed convert_time

--- rplugin/python3/vimtitles.py
+++ rplugin/python3/vimtitles.py
@@ -1,7 +1,7 @@
 import pynvim
 import pathlib
+import datetime
 from player import Player
-from convert_time import convert_time
 
 @pynvim.plugin
 class VimtitlesPlugin(object):
@@ -15,6 +15,22 @@ class VimtitlesPlugin(object):
         ts = self.nvim.funcs.searchpos('00:00', 'bn')
         ln = ts[0] - 1
         self.nvim.funcs.echo(buffer[ln])
+
+
+
+def convert_time(input):
+    """method for converting .srt time strings to number of seconds"""
+    if isinstance(input, float):
+        time = str(datetime.timedelta(seconds=input))
+        time.replace(".", ",")  # second to milisecond separator is a comma in .srt
+        time = time[:-3]
+        return(time)
+    elif isinstance(input, str):
+        time = input
+        time_struct = datetime.datetime.strptime(input, "%H:%M:%S,%f")
+        td = time_struct - datetime.datetime(1900, 1, 1)
+        time_float = td.total_seconds()
+        return(time_float)
 
 
 
