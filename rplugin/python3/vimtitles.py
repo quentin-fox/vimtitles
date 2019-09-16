d0b865e078706db62a4549bccf107c418dff2baa
Quentin Fox <quentinbennettfox@gmail.com>
2019-09-16 01:56:28 -0400

    intentionally breaking some things

--- rplugin/python3/vimtitles.py
+++ rplugin/python3/vimtitles.py
@@ -179,7 +179,7 @@ class VimtitlesPlugin(object):
 
     @pynvim.command('ShiftSubs', nargs=1)
     def shift_subs(self, args):
-        shift = float(args[0])
+        shift = 5
         buffer = self.nvim.current.buffer
         tsformat = '^\\d\\d:\\d\\d:\\d\\d,\\d\\d\\d --> \\d\\d:\\d\\d:\\d\\d,\\d\\d\\d\\s?$'
         ts_list = [(i, x.split(' ')[0], x.split(' ')[2]) for i, x in enumerate(buffer) if bool(re.match(tsformat, x))]
